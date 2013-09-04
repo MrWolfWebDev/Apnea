7192bf09a5a8e556a1e67fd935897152c704ee70
Andrea Valenza <avalenza89@gmail.com>
2013-09-04 18:09:56 +0200

    Added Database class (+ minor fixes)

--- php/db.class.php
+++ php/db.class.php
@@ -9,6 +9,7 @@ class Database {
     private $dbname = DB_NAME;
     private $dbh;
     private $error;
+    private $stmt;
 
     public function __construct() {
         // Set DSN
@@ -27,6 +28,70 @@ class Database {
         catch (PDOException $e) {
             $this->error = $e->getMessage();
         }
+    }
+
+    public function query($query) {
+        $this->stmt = $this->dbh->prepare($query);
+    }
+
+    public function bind($param, $value, $type = NULL) {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute() {
+        return $this->stmt->execute();
+    }
+
+    public function resultset() {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function single() {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // Number of affected rows
+    public function rowCount() {
+        return $this->stmt->rowCount();
+    }
+
+    public function lastInsertId() {
+        return $this->dbh->lastInsertId();
+    }
+
+    // Transaction methods
+    public function beginTransaction() {
+        return $this->dbh->beginTransaction();
+    }
+
+    public function endTransaction() {
+        return $this->dbh->commit();
+    }
+
+    public function cancelTransaction() {
+        return $this->dbh->rollBack();
+    }
+
+    public function debugDumpParams() {
+        return $this->stmt->debugDumpParams();
     }
 
 }
--- prova.php
+++ prova.php
@@ -1,10 +1,23 @@
 <?php
 
-// Include Database class
-include 'php/db.class.php';
-
 // Include Database config
 include 'php/dbconnection.php';
 
+// Include Database class
+include 'php/db.class.php';
+
+
 $database = new Database();
 
+$database->query('INSERT INTO mytable (FName, LName, Age, Gender) VALUES (:fname, :lname, :age, :gender)');
+
+$database->bind(':fname', 'John');
+$database->bind(':lname', 'Smith');
+$database->bind(':age', '24');
+$database->bind(':gender', 'male');
+
+$database->execute();
+
+echo $database->lastInsertId();
+echo $database->resultset();
+?>
